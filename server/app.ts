5aa6078a954cc3347d86ad2c928f495a5c68956a
esponges <esponges@gmail.com>
2023-07-29 13:18:02 -0600

    madre progress with erro now we're getting compile errors

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from 'express';
 import * as http from 'http';
 import next, { NextApiHandler } from 'next';
 
+// const port: number = parseInt('3030', 10);
 const port: number = parseInt(process.env.PORT || '3030', 10);
 const dev: boolean = process.env.NODE_ENV !== 'production';
 const nextApp = next({ dev });
